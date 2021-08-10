598c81cf6648a92ed88ce94b19dfd34902225eb4
Saksham003 <verms3saksham@gmail.com>
2021-08-10 14:32:17 +0530

    added dashboard and sign out button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DashBoard from '../components/DashBoard';
+import SideBar from '../components/SideBar';
+import { auth } from '../misc/firebase';
 
 const Home = () => {
-  return <div>This is Home</div>;
+  const [isDashboard, setIsDashboard] = useState(false);
+
+  const showDashboard = () => {
+    setIsDashboard(true);
+  };
+
+  const closeDashboard = () => {
+    setIsDashboard(false);
+  };
+
+  const onSignOut = () => {
+    auth.signOut();
+    closeDashboard();
+    alert('Signed out!');
+  };
+
+  return (
+    <div className="home__wrapper">
+      {isDashboard && (
+        <DashBoard onCloseDashboard={closeDashboard} onSignOut={onSignOut} />
+      )}
+      <div className="home">
+        <SideBar onShowDashboard={showDashboard} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
